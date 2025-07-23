bc0b598043441708ea23388351f734c37deda9a1
Yean <taingchongyean@gmail.com>
2025-07-23 21:39:10 +0700

    hmmmmm stock

--- src/main/java/com/yean/demo/controller/StockController.java
+++ src/main/java/com/yean/demo/controller/StockController.java
@@ -1,4 +1,26 @@
 package com.yean.demo.controller;
 
 public class StockController {
+    @Autowired
+    private StockService stockService;
+
+    @GetMapping
+    public ResponseEntity<BaseResponseWithDataModel> listStocks() {
+        return stockService.listStocks();
+    }
+
+    @PostMapping
+    public ResponseEntity<BaseResponseModel> createStock(@RequestBody StockModel payload) {
+        return stockService.createStock(payload);
+    }
+
+    @PatchMapping("{id}")
+    public ResponseEntity<BaseResponseModel> adjustQuantity(@PathVariable("id") Long stockId,@RequestBody UpdateStockModel payload) {
+        return stockService.adjustQuantity(stockId,payload);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<BaseResponseModel> deleteStock(@PathVariable("id") Long stockId) {
+        return stockService.deleteStock(stockId);
+    }
 }
