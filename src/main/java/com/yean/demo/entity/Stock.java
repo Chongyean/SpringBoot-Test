1cd1023845a4a3c7a31c4048155800bab9951c0a
Yean <taingchongyean@gmail.com>
2025-07-23 21:38:18 +0700

    same here

--- src/main/java/com/yean/demo/entity/Stock.java
+++ src/main/java/com/yean/demo/entity/Stock.java
@@ -1,4 +1,18 @@
 package com.yean.demo.entity;
 
 public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    private Integer quantity;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
